44c7ec84749318781765a4e599182477549e7eb8
Mike Feist <mdfeist@ualberta.ca>
2019-07-06 02:08:06 -0600

    Move test code for team creation to tournaments. Add function to randomly suffle subs into teams so that flex players are not unfairly favoured to be a sub.

--- src/tournament.js
+++ src/tournament.js
@@ -1,3 +1,5 @@
+const AsciiTable = require('ascii-table');
+
 const dateFormat = require('dateformat');
 const DB = require('./db');
 
@@ -10,16 +12,81 @@ module.exports.TOURNAMENT_STATE = {
   FINISHED: 4
 };
 
+// Get Fake Users SR for Testing
+function randn_sr() {
+  var u = 0, v = 0;
+  while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+  while(v === 0) v = Math.random();
+  let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) return randn_sr(); // resample between 0 and 1
+  return Math.round(5000.0*num);
+}
+
+function getRole(roles, role_id) {
+  return (roles >>> role_id) & 1;
+}
+
 function bitCount(n) {
   n = n - ((n >> 1) & 0x55555555)
   n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
   return ((n + (n >> 4) & 0xF0F0F0F) * 0x1010101) >> 24
 }
 
+function permutations(xs) {
+  let ret = [];
+
+  for (let i = 0; i < xs.length; i = i + 1) {
+    let rest = permutations(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+    if(!rest.length) {
+      ret.push([xs[i]])
+    } else {
+      for(let j = 0; j < rest.length; j = j + 1) {
+        ret.push([xs[i]].concat(rest[j]))
+      }
+    }
+  }
+  return ret;
+}
+
+function valid_permutations(player_pool) {
+  function _valid_permutations(player_pool, xs, role = 0) {
+    let ret = [];
+
+    let max = Math.floor(player_pool.length / 6);
+
+    for (let i = 0; i < xs.length; i = i + 1) {
+      let player = player_pool[xs[i]];
+      if (role < max) {
+        if (getRole(player.role, role % 6) == 0) {
+          continue;
+        }
+      }
+
+      let rest = _valid_permutations(player_pool, xs.slice(0, i).concat(xs.slice(i + 1)), role + 1);
+
+      if(!rest.length) {
+        ret.push([xs[i]])
+      } else {
+        for(let j = 0; j < rest.length; j = j + 1) {
+          ret.push([xs[i]].concat(rest[j]))
+        }
+      }
+    }
+
+    return ret;
+  }
+
+  let xs = [...Array(player_pool.length).keys()];
+  let valid = _valid_permutations(player_pool, xs);
+  return valid;
+}
+
 class Team {
   constructor(id) {
     this.id = id;
-    this.players = new Map();
+    this.players = [];
   }
 
   getID() {
@@ -31,58 +98,394 @@ class Team {
   }
 
   addPlayer(user) {
-    this.players.set(user.discord_id, user);
+    this.players.push(user);
   }
 
-  getPlayer(discord_id) {
-    return this.players.get(discord_id);
+  setPlayerAt(user, pos) {
+    this.players[pos] = user;
+  }
+
+
+  getPlayerByID(discord_id) {
+    return this.players.find(player => player.discord_id == discord_id);
+  }
+
+  getPlayerInPosition(pos) {
+    return this.players[pos];
+  }
+
+  getPlayers() {
+    return [...this.players];
   }
 
   isPlayerOnTeam(discord_id) {
-    return this.players.has(discord_id);
+    return this.getPlayerByID(discord_id) != null;
   }
 
   removePlayer(discord_id) {
-    this.players.delete(discord_id);
+    let player = this.getPlayerByID(discord_id);
+    if (player) {
+      let index = this.players.indexOf(player);
+      this.players.splice(index, 1);
+    }
   }
 
   getPlayerCount() {
-    return this.players.size;
+    return this.players.length;
+  }
+
+  // Number of players that can play
+  // this role
+  getRoleCount(role) {
+    let count = 0;
+    for (var player of this.players) {
+      if (player.role & (1 << role)) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+
+  getRoles() {
+    let roles = 0;
+    for (var player of this.players) {
+      roles |= player.role;
+    }
+
+    return roles;
   }
 
   getMissingRoles() {
-    let roles = 0;
-    for (var player of this.players) {
-      roles |= player.role;
-    }
-
-    return ~roles;
+    return ~this.getRoles() & 0x3F;
   }
 
-  getMissingRolesWithFlex() {
-    let roles = 0;
-    for (var player of this.players) {
-      roles |= player.role;
-    }
+  // Returns an array of the valid compositions the teams can play
+  // Each row represents the roles.
+  //
+  // Column 0 - Main Tank
+  // Column 1 - Off Tank
+  // Column 2 - Histscan DPS
+  // Column 3 - Porjectile DPS
+  // Column 4 - Main Support
+  // Column 5 - Off Support
+  //
+  // The value in the column is the player playing that role. The
+  // value represents the players position in the players array.
+  validTeamComps(number_of_players = 6) {
+    // Get all permutations of team
+    let valid = permutations([...Array(number_of_players).keys()]);
 
-    return ~roles;
+    // Can person even play the role given to them
+    valid = valid.filter(comp => {
+      for (let i = 0; i < 6; i++) {
+        let p = comp[i];
+        if (p < this.players.length) {
+          if (getRole(this.players[p].role, i) == 0) {
+            return false;
+          }
+        }
+      }
+      return true;
+    });
+
+    return valid;
   }
 
-  getAverageSR() {
+
+  getMinSR() {
+    let min_sr = 5000;
+    for (var player of this.players) {
+      if (min_sr > player.sr) {
+        min_sr = player.sr;
+      }
+    }
+
+    return min_sr;
+  }
+
+  getMaxSR() {
+    let max_sr = 0;
+    for (var player of this.players) {
+      if (max_sr < player.sr) {
+        max_sr = player.sr;
+      }
+    }
+
+    return max_sr;
+  }
+
+  getTotalSR() {
     let sr = 0;
     for (var player of this.players) {
       sr += player.sr;
     }
 
-    return sr / this.players.size;
+    return sr;
+  }
+
+  getAverageSR() {
+    return Math.round(this.getTotalSR() / this.players.length);
+  }
+
+  table() {
+    // Sort by rows
+    var table = new AsciiTable(`Team ${this.id}`)
+    table.setHeading('', '0', '1', '2', '3', '4', '5');
+    let row = 0;
+    for (var player of this.players) {
+      let r = player.role;
+      table.addRow(row, getRole(r, 0), getRole(r, 1), getRole(r, 2), getRole(r, 3), getRole(r, 4), getRole(r, 5));
+      row++;
+    }
+
+    return table;
   }
 
   toString() {
-    let team_string = this.getName() + `:\n`
+    let string = `${this.getName()}\n`;
     for (var player of this.players) {
-      team_string += player.bnet + ' ' + player.role;
+      let r = player.role;
+      string += `${player.bnet} - '${getRole(r, 0)}${getRole(r, 1)}${getRole(r, 2)}${getRole(r, 3)}${getRole(r, 4)}${getRole(r, 5)}\n`;
+    }
+    return string;
+  }
+}
+
+function createTeamsFromPool(player_pool) {
+  let teams = [];
+  let current_team_id = 1;
+  let current_team = new Team(current_team_id);
+
+  player_pool = [...player_pool];
+
+  while (player_pool.length > 0) {
+    console.log(`Creating new team with remaining ${player_pool.length} players`);
+
+    // All roles is 63
+    let missing_roles = 63;
+
+    // Fill missing roles
+    while (missing_roles) {
+      // Get the missing roles
+      missing_roles = current_team.getMissingRoles();
+
+      // Find player to fill role
+      let possible_players = player_pool.filter(player => player.role & missing_roles);
+
+      // If no player can fill the role then stop
+      if (possible_players.length == 0) {
+        break;
+      }
+
+      // Sort so players with the least amount of flex are picked first
+      possible_players.sort(function(a, b) {return bitCount(a.role) - bitCount(b.role)});
+
+      // Get player
+      let player = possible_players[0];
+
+      // Add player to team
+      current_team.addPlayer(player);
+
+      // Remove player from player pool
+      let index = player_pool.indexOf(player);
+      player_pool.splice(index, 1);
+    }
+
+    // If all roles are filled but the team doesn't
+    // have 6 players then find roles that a single player
+    // currently on the team fills mutiple of
+    while (current_team.getPlayerCount() < 6) {
+      // Get valid comps the team can play
+      let valid_comps = current_team.validTeamComps();
+      let player_number = current_team.getPlayerCount();
+
+      let valid_roles = 0;
+      for (let comp of valid_comps) {
+        let role = comp.indexOf(player_number);
+        valid_roles |= 1 << role;
+      }
+
+      // Find possible players to fill missing roles
+      let possible_players = player_pool.filter(player => player.role & valid_roles);
+
+      // If no player can fill the role then stop
+      if (possible_players.length == 0) {
+        break;
+      }
+
+      // Sort so players with the least amount of flex are picked first
+      possible_players.sort(function(a, b) {return bitCount(a.role) - bitCount(b.role)});
+
+      // Get player
+      let player = possible_players[0];
+
+      // Add player to team
+      current_team.addPlayer(player);
+
+      // Remove player from player pool
+      let index = player_pool.indexOf(player);
+      player_pool.splice(index, 1);
+    }
+
+    if (current_team.getPlayerCount() < 6) {
+      break;
+    }
+
+    teams.push(current_team);
+
+    current_team_id++;
+    current_team = new Team(current_team_id);
+  }
+
+  return teams;
+}
+
+function randomSwapWithSubs(teams, subs) {
+  let num_of_players = 6*teams.length + subs.length;
+  let swap_probability = 1.0 / num_of_players;
+  console.log(`Swap Probability: ${swap_probability}`);
+
+  for (let i = 0; i < subs.length; i = i + 1) {
+    // Get player on sub list
+    let sub = subs[i];
+
+    // Get all roles the player plays
+    let roles = [];
+    for (let role = 0; role < 6; role = role + 1) {
+      if (getRole(sub.role, role)) {
+        roles.push(role);
+      }
+    }
+
+    // Get randome role
+    let role = roles[Math.floor(Math.random()*roles.length)];
+
+    // Get random team
+    let team = teams[Math.floor(Math.random()*teams.length)];
+    let valid_comp = team.validTeamComps();
+
+    let team_player_pos = valid_comp[0][role];
+    let team_player = team.getPlayerInPosition(team_player_pos);
+
+    // Swap
+    if (Math.random() > swap_probability) {
+      team.setPlayerAt(sub, team_player_pos);
+      subs[i] = team_player;
     }
   }
+}
+
+function balanceTeams(teams, subs) {
+  // Balanace Teams by swapping players between teams
+  for (var ti = 0; ti < 2*teams.length; ti = ti + 1) {
+    teams.sort(function(a,b) {return a.getAverageSR() - b.getAverageSR()});
+    let lowest_sr_team = teams[0];
+    let highest_sr_team = teams[teams.length-1];
+
+    let l_total_sr = lowest_sr_team.getTotalSR();
+    let h_total_sr = highest_sr_team.getTotalSR();
+
+    let l_team_comp = lowest_sr_team.validTeamComps();
+    let h_team_comp = highest_sr_team.validTeamComps();
+
+    // Get lowest sr player from lowest sr team and their role
+    let l_player_pos = 0;
+    let l_sr_player = 5000;
+    let l_player_role = 0;
+    for (var role = 0; role < l_team_comp[0].length; role = role + 1) {
+      let player_pos = l_team_comp[0][role];
+      let player = lowest_sr_team.getPlayerInPosition(player_pos);
+      if (player.sr < l_sr_player) {
+        l_sr_player = player.sr;
+        l_player_pos = player_pos;
+        l_player_role = role;
+      }
+    }
+
+    let l_player_roles = [l_player_role];
+    for (var comp = 1; comp < l_team_comp.length; comp = comp + 1) {
+      for (var role = 0; role < l_team_comp[comp].length; role = role + 1) {
+        let player_pos = l_team_comp[comp][role];
+        if (player_pos == l_player_pos) {
+          l_player_roles.push(role);
+          break;
+        }
+      }
+    }
+
+    //If lowest sr player lower than player in higher team swap
+    let current_sr_diff =  Math.abs(h_total_sr - l_total_sr);
+
+    // Get lowest sr player
+    let l_player = lowest_sr_team.getPlayerInPosition(l_player_pos);
+    let best_h_player = null;
+    let best_sr_diff = 5000;
+    let best_swap_player_pos = 0;
+
+    for (var role = 0; role < l_player_roles.length; role = role + 1) {
+      for (var comp = 0; comp < h_team_comp.length; comp = comp + 1) {
+        // Get player with equivalent role in higher sr teams
+        let swap_player_pos = h_team_comp[comp][l_player_roles[role]];
+        let h_player = highest_sr_team.getPlayerInPosition(swap_player_pos);
+
+        let l_total_sr_if_swap = (l_total_sr - l_player.sr) + h_player.sr;
+        let h_total_sr_if_swap = (h_total_sr - h_player.sr) + l_player.sr;
+
+        let swap_sr_diff = Math.abs(h_total_sr_if_swap - l_total_sr_if_swap);
+
+        if (swap_sr_diff < best_sr_diff) {
+          best_sr_diff = swap_sr_diff;
+          best_h_player = h_player;
+          best_swap_player_pos = swap_player_pos;
+        }
+      }
+    }
+
+    // Swap if better than current
+    if (best_sr_diff < current_sr_diff) {
+      lowest_sr_team.setPlayerAt(best_h_player, l_player_pos);
+      highest_sr_team.setPlayerAt(l_player, best_swap_player_pos);
+    } else {
+      // TODO: If not then move on to next sr player
+      break;
+    }
+  }
+}
+
+function getSubPool(teams, player_pool) {
+  let subs = [];
+  for (var i = 0; i < player_pool.length; i++) {
+    console.log(i);
+    var found = false;
+    let player = player_pool[i];
+    for (var team of teams) {
+      if (team.isPlayerOnTeam(player.discord_id)) {
+        console.log(`found ${player.discord_id} on team ${team.id}`);
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      subs.push(player);
+    }
+  }
+
+  return subs;
+}
+
+function printPool(player_pool) {
+  var table = new AsciiTable(`Players`)
+  table.setHeading('', '0', '1', '2', '3', '4', '5');
+  let row = 0;
+  for (var player of player_pool) {
+    let r = player.role;
+    table.addRow(row, getRole(r, 0), getRole(r, 1), getRole(r, 2), getRole(r, 3), getRole(r, 4), getRole(r, 5));
+    row++;
+  }
+
+  return table;
 }
 
 module.exports.createTournament = async function(json) {
@@ -195,6 +598,9 @@ function Tournament(params)
   this.checkin_message = params.checkin_message;
 
   this.teams = [];
+  this.subs = [];
+
+  this.invalid_users = [];
 
   this.post = async function(channel) {
     if (!channel) {
@@ -233,11 +639,57 @@ function Tournament(params)
     await DB.database().runAsync(updateSql);
   }
 
-  this.createTeams = async function() {
-    // During testing make sure you
-    // have at least this many players
-    const testing_player = 60;
+  this.testCreateTeams = async function(testing_player) {
+    let player_pool = [];
 
+    // Create Player Pool
+    for (var i = 0; i < testing_player; i++) {
+      let bnet = `FakeTester#${i}`;
+      let sr = randn_sr();
+      let role = 0;
+
+      let num_of_roles = Math.floor(Math.random() * 3) + 1;
+
+      for (var r = 0; r < num_of_roles; r++) {
+        role |= (1 << Math.floor(Math.random() * 6));
+      }
+
+      let user = {
+        bnet: bnet,
+        discord_id: 'discord_' + bnet,
+        sr: sr,
+        role: role
+      };
+
+      player_pool.push(user);
+    }
+
+    this.teams = createTeamsFromPool(player_pool);
+    this.subs = getSubPool(this.teams, player_pool);
+    randomSwapWithSubs(this.teams, this.subs);
+    balanceTeams(this.teams, this.subs);
+
+    for (var team of this.teams) {
+      console.log(`${team.getName()}: (${team.getMinSR()}, ${team.getMaxSR()}) -  ${team.getAverageSR()}`);
+    }
+
+    for (var team of this.teams) {
+      console.log(team.toString());
+      console.log(team.validTeamComps());
+    }
+
+    console.log("--------------------------------");
+    console.log("--------      SUBS      --------");
+    console.log("--------------------------------");
+    let sub_team = new Team(this.teams.length+1);
+    for (var sub of this.subs) {
+      sub_team.addPlayer(sub);
+    }
+
+    console.log(sub_team.toString());
+  }
+
+  this.createTeams = async function() {
     var sql = `SELECT * FROM Users WHERE discord_id = (SELECT discord_id FROM Users_Tournaments WHERE tournament_id = '${this.id}')`;
     let users = await DB.database().allAsync(sql);
 
@@ -246,87 +698,14 @@ function Tournament(params)
     }
 
     let valid_users = users.filter(isValid);
-    let invalid_users = users.filter(!isValid);
+    this.invalid_users = users.filter(!isValid);
 
     //TODO: Message invalid users
 
-    //Add Fake Users for Testing
-    function randn_sr() {
-      var u = 0, v = 0;
-      while(u === 0) u = Math.random(); //Converting [0,1) to (0,1)
-      while(v === 0) v = Math.random();
-      let num = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
-      num = num / 10.0 + 0.5; // Translate to 0 -> 1
-      if (num > 1 || num < 0) return randn_sr(); // resample between 0 and 1
-      return Math.round(5000.0*num);
-    }
-
-    for (var i = valid_users.length; i < testing_player; i++) {
-      let bnet = `FakeTester#${i}`;
-      let sr = randn_sr();
-      let role = 0;
-
-      let num_of_roles = Math.floor(Math.random() * 3) + 1;
-
-      for (var i = 0; i < num_of_roles; i++) {
-        role |= (1 << Math.floor(Math.random() * 6));
-      }
-
-      let user = {
-        bnet: bnet,
-        discord_id: null,
-        sr: sr,
-        role: role
-      };
-
-      valid_users.push(user);
-    }
-
-    //TODO: Create teams
-    let player_pool = [...valid_users];
-    let current_team_id = 1;
-    let current_team = new Team(current_team_id);
-
-    while (player_pool.length > 0) {
-      // All roles is 63
-      let missing_roles = 63;
-
-      // Fill missing roles
-      while (missing_roles) {
-        // Get the missing roles
-        missing_roles = current_team.getMissingRoles();
-
-        // Find player to fill role
-        let possible_players = player_pool.filter(player => player.role & missing_roles);
-
-        // If no player can fill the role then stop
-        if (possible_players.length == 0) {
-          break;
-        }
-
-        // Sort so players with the least amount of flex are picked first
-        possible_players.sort(function(a, b) {return bitCount(a) - bitCount(b)});
-
-        // Get player
-        let player = possible_players[0];
-
-        // Add player to team
-        current_team.addPlayer(player);
-
-        // Remove player from player pool
-        let index = player_pool.indexOf(player);
-        player_pool.splice(index, 1);
-      }
-
-      // TODO: If all roles are filled but the team doesn't
-      // have 6 players then find roles that a single player
-      // currently on the team fills mutiple of
-      while (current_team.getPlayerCount() < 6) {
-
-      }
-    }
-
-    //TODO: Create balanced teams based of role and sr
+    this.teams = createTeamsFromPool(valid_users);
+    this.subs = getSubPool(this.teams, valid_users);
+    randomSwapWithSubs(this.teams, this.subs);
+    balanceTeams(this.teams, this.subs);
   }
 
   this.save = async function() {
